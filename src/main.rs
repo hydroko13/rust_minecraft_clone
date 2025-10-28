cd625c8d15472e2ef2cfc02d5738c3653de4052b
Maxim Kozlov <maximko.ca@gmail.com>
2025-10-28 07:55:19 -0400

    Change window size

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 use glfw::{Action, Context, Key};
+use gl::*;
 
 fn main() {
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
 
-    let (mut window, events) = glfw.create_window(1280, 800, "Hello this is window", glfw::WindowMode::Windowed)
+    let (mut window, events) = glfw.create_window(1280, 800, "Rust minecraft clone", glfw::WindowMode::Windowed)
         .expect("Failed to create GLFW window.");
 
     window.set_key_polling(true);
     window.make_current();
+
+    gl::load_with(|s| window.get_proc_address(s).unwrap() as *const _ );
+    
+
+    unsafe {
+        gl::Viewport(0, 0, 1280, 800);
+        gl::ClearColor(0.5, 0.0, 0.0, 1.0);
+    }
 
     while !window.should_close() {
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
             handle_window_event(&mut window, event);
         }
+
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT)
+        };
+
+        window.swap_buffers();
+
     }
 }
 
